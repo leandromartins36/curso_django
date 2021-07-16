009268a748a79c9b495b61f1662091cae320b9af
leandro de menezes martins <leandro.m.martins.info@gmail.com>
2021-07-16 10:21:13 -0300

    mudando a pasta base pra pypro
    mudando a pasta base pra pypro

--- pypro/urls.py
+++ pypro/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from base.views import home
+from pypro.base import home
 
 urlpatterns = {
     path('admin/', admin.site.urls),
